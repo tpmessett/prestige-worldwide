1d7f00e974488598061649ae775ff59a87e694a7
Dan Menza <dansmenza@gmail.com>
2021-02-22 17:49:17 +0000

    spacing on boookings_controller and visibility without login on pages_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,6 +17,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-      params.require(:booking).permit(:start_date, :end_date, :yacht_id, :user_id)
+    params.require(:booking).permit(:start_date, :end_date, :yacht_id, :user_id)
   end
 end
