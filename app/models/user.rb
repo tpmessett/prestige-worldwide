c6c7fb0142319d221de7bfbe3dc0fec8a8784c95
thomas messett <thomas.messett@stampapp.me>
2021-02-22 17:03:29 +0000

    added basic front end for home page including html and css

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   has_many :bookings
   has_many :yachts
+  has_many :incoming_bookings, through: :yachts, source: :bookings
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
