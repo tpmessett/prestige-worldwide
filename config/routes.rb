e4113f40372e454018a6d423cc003385b12cbb58
Dan Menza <dansmenza@gmail.com>
2021-02-26 15:01:07 +0000

    edit route now available. Updated yacht show description to not cut off

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :yachts, only: [:index, :show, :new, :create] do
+  resources :yachts, only: [:index, :show, :new, :create, :edit, :update] do
     resources :bookings, only: [:show, :new, :create]
   end
   resources :search_yacht, only: [:index]
